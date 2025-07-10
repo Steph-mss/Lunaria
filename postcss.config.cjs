ab40ec5b56d48163177938bd265d1e6b2a0e165e
L'ADMINISTRATEUR S <s.simonis@ecole-ipssi.net>
2025-07-10 19:11:43 +0200

    maj

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,12 +1,8 @@
-// postcss.config.cjs
 module.exports = {
   plugins: {
-    // anciennement : tailwindcss: {},
-    '@tailwindcss/postcss': {},
-    autoprefixer: {},
-
-    // si vous aviez d'autres plugins, conservez-les ici :
-    '@csstools/postcss-global-data': { files: ['app/global.module.css'] },
+    '@csstools/postcss-global-data': {
+      files: ['app/global.module.css'],
+    },
     'postcss-custom-media': {},
   },
 };
